35ef81469bf43d8eb3b0e41ce2f464aa977ac427
Benjamin Kim <benkim96@gmail.com>
2020-09-20 18:16:42 -0700

    skeleton with routing

---
+++ src/components/layout/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default function Header() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="flex">
+          <div>
+            <Link to="/" className="brand">
+              Ben Kim
+            </Link>
+          </div>
+          <div className="flex">
+            <Link to="/">Me</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/resume">Resume</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
