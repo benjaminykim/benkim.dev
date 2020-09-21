5797cadea0a1db16c21bc7ac6f1d7f2adc4589fe
Benjamin Kim <benkim96@gmail.com>
2020-09-20 22:41:20 -0700

    make project desc more concise

--- src/data/project.js
+++ src/data/project.js
@@ -5,7 +5,7 @@ export default [
     path: 'https://www.thanktank.app',
     source: 'https://github.com/thank-tank/',
     description:
-			'A social platform for expressing gratitude',
+			'social platform for gratitude',
 		stack:
 			'Ruby on Rails, Flutter, Dart, Heroku',
 	},
@@ -15,7 +15,7 @@ export default [
 		path: '',
 		source: 'https://github.com/cibic-io',
 		description:
-			'A social platform for political discourse and civic engagement.',
+			'social platform for political discourse and civic engagement.',
 		stack: 'Node, JS, TS, Flutter, Dart, AWS, Docker, BASH'
 	},
 	{
@@ -23,7 +23,7 @@ export default [
 		title: 'Co-Create Containers',
 		path: 'https://www.co-createcontainers.com/',
 		source: 'https://github.com/co-create',
-		description: 'A smart home IOT project to quantify the way we live',
+		description: 'smart home IOT project to quantify the way we live',
 		stack: 'React, React Native, JS, TS, Django, C, Arduino, BASH'
 	},
 	{
@@ -31,7 +31,7 @@ export default [
 		title: 'Autonomous Vehicle',
 		path: 'https://github.com/benjaminykim/self-driving-car-simulator',
 		source: 'https://github.com/benjaminykim/self-driving-car-simulator',
-		description: 'A trained neural model to steer a vehicle around a track.',
+		description: 'trained neural model to steer a vehicle around a track.',
 		stack: 'Python, Numpy, Tensorflow, Keras',
 	},
 	{
@@ -39,7 +39,7 @@ export default [
 		title: 'StockView',
 		path: 'https://benjaminykim.github.io/stock_view/',
 		source: 'https://github.com/benjaminykim/stock_view',
-		description: 'A site to explore stock graphs and descriptions',
+		description: 'react website to explore stock graphs and descriptions',
 		stack: 'React, JS',
 	},
 	{
@@ -47,7 +47,7 @@ export default [
 		title: 'Data Science Tutorial',
 		path: 'https://github.com/benjaminykim/python_data_science_tutorial',
 		source: 'https://github.com/benjaminykim/python_data_science_tutorial',
-		description: 'A tutorial hosted on Github for new developers and data scientists',
+		description: 'tutorial hosted for new developers and data scientists',
 		stack: 'Jupyter Notebook, Python, BASH, unix',
 	},
 ]
