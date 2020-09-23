8c8e18ddffb44d644a495aef80068988e3839edc
Benjamin Kim <benkim96@gmail.com>
2020-09-22 18:44:38 -0700

    update skill disclaimers

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,7 +10,6 @@ export default function Skills({ data }) {
 								<div className="skill-item">{skill}</div>
 					)
 				})}
-				<p class="skill-disclaimer">*If I had to select 5</p>
     	</div>
 			<p class="skill-title">Frontend</p>
 			<div className="skill-section">
@@ -43,7 +42,7 @@ export default function Skills({ data }) {
 								<div className="skill-item">{skill}</div>
 					)
 				})}
-			<p class="skill-disclaimer">*I consider myself to be mostly language agnostic</p>
+			<p class="skill-disclaimer">*language agnostic</p>
     	</div>
 			<p class="skill-title">Libraries/Tools</p>
 			<div className="skill-section">
