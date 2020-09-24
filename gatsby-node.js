447cbde9fd01a25341fc2402b6f932f14beaa866
Benjamin Kim <benkim96@gmail.com>
2020-09-24 01:02:08 -0700

    update schema

---
+++ gatsby-node.js
@@ -0,0 +1,41 @@
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+
+  const blogPostTemplate = require.resolve(`./src/templates/blogTemplate.js`)
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+		console.log(node.frontmatter.slug);
+		console.log(node.frontmatter.title);
+    createPage({
+      slug: '/blog/' + node.frontmatter.slug,
+      component: blogPostTemplate,
+      context: {
+        // additional data can be passed via context
+        slug: '/blog/' + node.frontmatter.slug,
+      },
+    });
+  })
+}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout/Layout.js'
 import Helmet from 'react-helmet'
+import { Link } from 'gatsby';
 
 export default function Blog() {
   return (
@@ -10,6 +11,10 @@ export default function Blog() {
 				<br/>
 				<br/>
 				<p>Coming Soon!</p>
+				<main>
+    Sourcing markdown recipe (see page at{` `}
+    <Link to="vscode-gatsby-bug.js">/my-first-post</Link>)
+  </main>
 			</div>
 		</Layout>
 	)
