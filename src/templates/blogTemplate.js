68ee6d0673cfd4c54199ff47b7eeaa06d5b72c38
Benjamin Kim <benkim96@gmail.com>
2020-09-25 00:53:23 -0700

    add disqus components to blogTemplate

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -3,12 +3,18 @@ import { graphql } from "gatsby"
 import Helmet from 'react-helmet'
 import Layout from '../components/layout/Layout'
 import { formatDate } from '../utils/helpers'
+import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
 
 export default function Template({
   data,
 }) {
 	const { markdownRemark } = data
-  const { frontmatter, html } = markdownRemark
+	const { frontmatter, html } = markdownRemark
+	let disqusConfig = {
+    identifier: frontmatter.path,
+    title: frontmatter.title,
+	}
+
   return (
 		<Layout>
 		<Helmet title={`${frontmatter.title} | Ben Kim`} />
@@ -20,10 +26,12 @@ export default function Template({
 		<section>
 			<div className="container medium">
         <p className="post-preview-date" >Published: {formatDate(frontmatter.date)}</p>
+				<CommentCount config={disqusConfig} placeholder={'...'} />
         <div
           className="post-preview-description"
           dangerouslySetInnerHTML={{ __html: html }}
         />
+      	<Disqus config={disqusConfig} />
 			</div>
 		</section>
 	</Layout>
